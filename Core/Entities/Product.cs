17a1eb6350ba89e2f3b2b36ea02205a6e280ecfa
Yizhi Liang <yizhiliang@Yizhis-MacBook-Pro.local>
2021-02-01 13:34:58 -0800

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        // properties: Id, Name
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
