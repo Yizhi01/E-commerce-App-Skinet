2f3666ddf419dafe8c2b0eeea7ed9ad1604b0148
Yizhi Liang <yizhiliang@Yizhis-MacBook-Pro.local>
2021-02-02 00:19:24 -0800

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,42 +9,44 @@ using Microsoft.Extensions.Hosting;
 
 namespace API
 {
-  public class Startup
-  {  
-    private readonly IConfiguration _config; // Define private field with an underscore
-    public Startup(IConfiguration config)
+    public class Startup
     {
-      _config = config;
+        private readonly IConfiguration _config; // Define private field with an underscore
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddControllers();
+            // Add the DbContext 
+            services.AddDbContext<StoreContext>(
+                x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddControllers();
-      services.AddDbContext<StoreContext>(
-          x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
 }
